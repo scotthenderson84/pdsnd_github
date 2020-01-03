6d67ad5d201b3564a15426e249f660e734a11512
Henderson <Scott.S.Henderson@shell.com>
2020-01-03 20:14:53 +0000

    Style: Fixed spelling error for City data #DE1234

--- Project2 - BikeShareScottHenderson.py
+++ Project2 - BikeShareScottHenderson.py
@@ -45,7 +45,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the Specified city and filters by month and day if applicable.
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
